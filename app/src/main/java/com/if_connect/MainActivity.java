6c4ab925b6efa1e7926ce79e62fd36d1f70b6968
Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
2024-02-17 20:08:27 -0300

    Initial commit

---
+++ app/src/main/java/com/if_connect/MainActivity.java
@@ -0,0 +1,16 @@
+package com.if_connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.if_connect.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "if-connect"
+include(":app")
